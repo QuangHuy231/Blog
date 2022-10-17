3caa6180596eab3a4b7e120142068261f16cf513
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-10-17 18:34:33 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import Single from "./views/Single";
+import Write from "./views/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Fragment } from "react";
+
+const Layout = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </Fragment>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  // {
+  //   path: "/register",
+  //   element: <Register />,
+  // },
+]);
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
