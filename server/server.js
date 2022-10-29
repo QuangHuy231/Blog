36d9d4953113fceede2a6fa7d650ea6fd8aa9107
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-10-29 21:52:28 +0700

    Login use token save in cookies

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
@@ -6,6 +7,7 @@ import authRoutes from "./routes/auth.js";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
